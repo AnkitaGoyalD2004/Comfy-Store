8458a7291c9489ef4406758634eda4d2aed3b559
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-16 13:42:41 +0530

    done

--- comfyStore/src/Components/HomeLayout.jsx
+++ comfyStore/src/Components/HomeLayout.jsx
@@ -1,16 +1,31 @@
+// import { Outlet } from "react-router-dom";
+// import Header from "./components/Header";
+// import Navbar from "./components/Navbar";
+// const HomeLayout = () => {
+//   return (
+//     <>
+//       <nav>
+//         <Header />
+//         <Navbar />
+//       </nav>
+
+//       <section className="align-element py-20">
+//         <Outlet />
+//       </section>
+//     </>
+//   );
+// };
+// export default HomeLayout;
+
 import { Outlet, useNavigation } from "react-router-dom";
-
-import Navbar from "./Navbar";
-import Loading from "./Loading";
-import Header from "./Header";
-
+import { NavBar, Loading, Header } from "../Components";
 const HomeLayout = () => {
   const navigation = useNavigation();
   const isPageLoading = navigation.state === "loading";
   return (
     <>
-      <Header />
-      <Navbar />
+      <Header></Header>
+      <NavBar />
       {isPageLoading ? (
         <Loading />
       ) : (
