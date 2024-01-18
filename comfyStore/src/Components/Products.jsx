54eef31f3c0bdb35d41e63d719c39ccdcd04326d
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-18 13:57:20 +0530

    doen

--- comfyStore/src/Components/Products.jsx
+++ comfyStore/src/Components/Products.jsx
@@ -1,29 +1,3 @@
-// import { ProductsContainer, PaginationContainer, Filters } from "../Components";
-// import { customFetch } from "../utils";
-
-// const url = "/products";
-// export const loader = async ({ request }) => {
-//   const params = Object.fromEntries([
-//     ...new URL(request.url).searchParams.entries(),
-//   ]);
-//   console.log(params);
-//   const response = await customFetch(url, { params });
-
-//   const products = response.data.data;
-//   const meta = response.data.meta;
-//   return { products, meta, params };
-// };
-
-// const Products = () => {
-//   return (
-//     <>
-//       <Filters />
-//       <ProductsContainer />
-//       <PaginationContainer />
-//     </>
-//   );
-// };
-// export default Products;
 import { Filters, PaginationContainer, ProductsContainer } from "../Components";
 import { customFetch } from "../utils";
 const url = "/products";
--- comfyStore/src/store.js
+++ comfyStore/src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-import cartReducer from "../features/cart/cartSlice";
-import userReducer from "../features/users/userSlice";
+import cartReducer from "./features/cart/cartSlice";
+import userReducer from "./features/users/userSlice";
 export const store = configureStore({
   reducer: {
     cartState: cartReducer,
