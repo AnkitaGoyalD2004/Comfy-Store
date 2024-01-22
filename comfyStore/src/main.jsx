41c0fa1fb0ffd9d697b7de100fb2d86800d94d48
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-22 09:38:21 +0530

    done

--- comfyStore/src/main.jsx
+++ comfyStore/src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "react-toastify/dist/ReactToastify.css";
-// order
-import "./index.css";
 
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 import { ToastContainer } from "react-toastify";
 import { store } from "./store.js";
 import { Provider } from "react-redux";
@@ -12,6 +11,6 @@ import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
-    <ToastContainer position="top-bottom" />
+    <ToastContainer position="top-center" />
   </Provider>
 );
