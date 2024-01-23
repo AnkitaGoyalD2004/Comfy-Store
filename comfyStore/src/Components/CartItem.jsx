50ef7fb29b63e1e041f92d192f745e5cd0d8bb6d
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-23 13:04:54 +0530

    complete

--- comfyStore/src/Components/CartItem.jsx
+++ comfyStore/src/Components/CartItem.jsx
@@ -1,36 +1,41 @@
-import { generateAmountOptions } from "../utils";
+import { formatPrice, generateAmountOptions } from "../utils";
 import { removeItem, editItem } from "../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
-
-export default function CartItem({ cartItem }) {
+const CartItem = ({ cartItem }) => {
   const dispatch = useDispatch();
-  const { cartID, title, price, image, amount, company, productColor } =
-    cartItem;
 
-  const removeItemFunc = () => {
+  const removeItemFromTheCart = () => {
     dispatch(removeItem({ cartID }));
   };
   const handleAmount = (e) => {
     dispatch(editItem({ cartID, amount: parseInt(e.target.value) }));
   };
 
+  const { cartID, title, price, image, amount, company, productColor } =
+    cartItem;
+
   return (
     <article
       key={cartID}
-      className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6"
+      className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0"
     >
+      {/* IMAGE */}
       <img
         src={image}
         alt={title}
         className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover"
       />
+      {/* INFO */}
       <div className="sm:ml-16 sm:w-48">
-        <h3 className="capitalize font-medium"> {title} </h3>
-        <h4 className="mt-2 capitalize text-sm text-natural-content">
+        {/* TITLE */}
+        <h3 className="capitalize font-medium">{title}</h3>
+        {/* COMPANY */}
+        <h4 className="mt-2 capitalize text-sm text-neutral-content">
           {company}
         </h4>
+        {/* COLOR */}
         <p className="mt-4 text-sm capitalize flex items-center gap-x-2">
-          color:{" "}
+          color :
           <span
             className="badge badge-sm"
             style={{ backgroundColor: productColor }}
@@ -38,28 +43,33 @@ export default function CartItem({ cartItem }) {
         </p>
       </div>
       <div className="sm:ml-12">
+        {/* AMOUNT */}
         <div className="form-control max-w-xs">
-          <label htmlFor="label" className="label p-0">
-            <span className="label-text">Amount:</span>
+          <label htmlFor="amount" className="label p-0">
+            <span className="label-text">Amount</span>
           </label>
           <select
             name="amount"
             id="amount"
-            className="mt-2 select select-base select-bordered select-sm"
+            className="mt-2 select select-base select-bordered select-xs"
+            value={amount}
             onChange={handleAmount}
-            defaultValue={amount}
           >
             {generateAmountOptions(amount + 5)}
           </select>
         </div>
+        {/* REMOVE */}
         <button
-          className="mt-2 link link-secondary link-hover text-sm"
-          onClick={removeItemFunc}
+          className="mt-2 link link-primary link-hover text-sm"
+          onClick={removeItemFromTheCart}
         >
-          Remove
+          remove
         </button>
       </div>
-      <p className="font-medium sm:ml-auto">₹ {price}</p>
+
+      {/* PRICE */}
+      <p className="font-medium sm:ml-auto">{formatPrice(price)}</p>
     </article>
   );
-}
+};
+export default CartItem;
