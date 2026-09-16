d7a566499ccbe933d3796f0707ff0757a0635339
Ankita Goyal <goyalankita0325@gmail.com>
2026-09-16 12:37:26 +0530

    Add vercel configuration and latest updates

--- comfyStore/src/Components/cartItemsList.jsx
+++ comfyStore/src/Components/cartItemsList.jsx
@@ -1,17 +1,4 @@
-// import { useSelector } from "react-redux";
-// import CartItem from "./CartItem";
 
-// const CartItemsList = () => {
-//   const cartItems = useSelector((state) => state.cartState.cartItems);
-//   return (
-//     <>
-//       {cartItems.map((item) => {
-//         return <CartItem key={item.cartID} cartItem={item} />;
-//       })}
-//     </>
-//   );
-// };
-// export default CartItemsList;
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 
